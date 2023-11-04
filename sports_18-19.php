60b00b33502e128cd2dc23be9d207489b4646447
Jothikap-cs21 <138756465+Jothikap-cs21@users.noreply.github.com>
2023-11-04 10:38:49 +0530

    jothika

--- sports_18-19.php
+++ sports_18-19.php
@@ -7,7 +7,7 @@ include("conn.php")
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Stella Mary's College of Engineering</title>
+  <title>Stella Mary's College of Engineering</title>  
   <meta content="" name="description">
   <meta content="" name="keywords">
 
